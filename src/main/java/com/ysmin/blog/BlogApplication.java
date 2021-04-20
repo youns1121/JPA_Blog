6d8066ad9beddfc0d639091a737537861d0dd848
younsikMin <minys94@daum.net>
2021-04-20 14:53:30 +0900

    환경세팅완료 v1

---
+++ src/main/java/com/ysmin/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.ysmin.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlogApplication.class, args);
+
+
+    }
+
+}
